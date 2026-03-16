0fd749730eeafe1f610144dd82b94999166a7365
Triinu Maran <triinumaran@gmail.com>
2026-03-16 11:16:20 +0200

    file structure and examples fix

--- routes/ticketroute.ts
+++ routes/ticketroute.ts
@@ -1,15 +1,15 @@
 import { 
-BuyTicket,
+createTicket,
 findTicketByEventIdService,
 deleteTicket,
 getUserTickets
- } from "../services/ticketServices";
+ } from "../services/ticketservices";
 import { NextFunction, Request, Response, Router } from "express";
 
 
 const EventTicketRouter = Router();
 
-EventTicketRouter.post("/create", BuyTicket);
+EventTicketRouter.post("/create", createTicket);
 EventTicketRouter.post("/event-tickets", findTicketByEventIdService);
 EventTicketRouter.delete("/delete/:id", deleteTicket);
 EventTicketRouter.get("/all", getUserTickets);
